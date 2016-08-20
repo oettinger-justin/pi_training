844f26b00358d76474cf20ddeb7b9a142ff4f7a9
Justin Oettinger <Justin Oettinger>
2016-08-20 15:49:54 -0400

    visforpoketrap

--- index.js
+++ index.js
@@ -2,19 +2,54 @@ var gpio = require('rpi-gpio');
 
 gpio.setMode(gpio.MODE_BCM);
 
-gpio.setup(18, gpio.DIR_OUT, setInterval.bind(null, write, 1000));
+gpio.setup(18, gpio.DIR_OUT, setInterval.bind(null, write.bind(null, 18), 791));
+gpio.setup(25, gpio.DIR_OUT, setInterval.bind(null, write.bind(null, 25), 791));
+gpio.setup(12, gpio.DIR_OUT, setInterval.bind(null, write.bind(null, 12), 791));
+gpio.setup(21, gpio.DIR_OUT, setInterval.bind(null, write.bind(null, 21), 791));
 
-let i = true
+let c = 0;
+let b = false;
 
-function write() {
-    gpio.write(18, i, function (err) {
+function write(x) {
+    a = c;
+    c = c++
+    gpio.write(x, condswitch(x), function (err) {
         if (err) throw err;
-        console.log('Written to pin');
-        if (i === true) {
-            i = false;
-        }
-        else {
-            i = true;
-        }
     });
 }
+
+function condswitch(x) {
+    if (x === 12) {
+        return (b = !b);
+    }
+    if (x === 18) {
+        if ((c % 2) === 0) {
+            return true;
+        }
+        else if ((c % 5) === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    if (x === 21) {
+        if (Math.round(Math.sqrt(c)) === (Math.sqrt(c))) {
+            return true;
+        }
+        else if ((c % 5) === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    if (x === 25) {
+        if ((c % 3) === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
